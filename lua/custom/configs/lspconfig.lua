0bc53270b5b9e54ddba10a7291fa3e6988b843a0
zachmahan <zach.mahan.zm@gmail.com>
2025-09-07 11:49:23 -0400

    add java-language-server, update mason ensure installed list

--- lua/custom/configs/lspconfig.lua
+++ lua/custom/configs/lspconfig.lua
@@ -18,6 +18,7 @@ local servers = {
     "marksman",
     --"asm_lsp", --good, but doesn't work w/ b16 so disable it for now
     "glsl_analyzer",
+    "java_language_server",
 }
 vim.lsp.enable(servers)
 -- override Pyright setup after the bulk enable to add custom config
@@ -55,6 +56,16 @@ lspconfig.clangd.setup {
         "--offset-encoding=utf-16", -- fix offset errors in some setups
     },
 }
+local home = os.getenv "HOME"
+local util = lspconfig.util
+lspconfig.java_language_server.setup {
+    cmd = { home .. "/.local/share/nvim/mason/bin/java-language-server" },
+    root_dir = util.root_pattern("pom.xml", "build.gradle", ".git"),
+    handlers = {
+        ["client/registerCapability"] = function() end, -- ignore dynamic registration warnings
+    },
+}
+
 -- not working as of 20250824
 lspconfig.racket_langserver.setup {
     cmd = { "racket", "-l", "racket-langserver" },
