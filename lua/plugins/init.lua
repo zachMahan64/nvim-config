f0156a31cb2b4838c3fe10c14809e1038d70d3f9
zachMahan64 <zach.mahan.zm@gmail.com>
2026-04-10 22:58:59 -0400

    update plugs

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -388,6 +388,15 @@ local plugs = {
         "HiPhish/rainbow-delimiters.nvim",
         enabled = false,
     },
+    {
+        "rachartier/tiny-inline-diagnostic.nvim",
+        event = "VeryLazy",
+        priority = 1000,
+        config = function()
+            require("tiny-inline-diagnostic").setup()
+            vim.diagnostic.config { virtual_text = false } -- Disable Neovim's default virtual text diagnostics
+        end,
+    },
 }
 
 return plugs
